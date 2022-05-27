96b331dbbb0f9ffea27b4c4223a8e9456ca65756
Lucho-lja <ljallauca.a@gmail.com>
2022-05-27 13:05:34 -0500

    Add identification to join and generate Boards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'boards/index'
+  get 'boards/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "boards#index"
 end
