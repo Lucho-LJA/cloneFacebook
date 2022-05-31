4e7b925c9e024b65ffa95948886d596bede15ef1
Lucho-lja <ljallauca.a@gmail.com>
2022-05-31 18:32:52 -0500

    Save with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
   
   def self.from_omniauth(auth)
+    p "holiholiholi"
+    p auth.info.name.split(" ")
+    p "holaholahola"
     name_split = auth.info.name.split(" ")
     user = User.where(email: auth.info.email).first
-    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0, 20])
       user
   end
 end
