196a9fbede2c9b25a5704e2a3f4abb24a4e598fb
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 15:56:10 -0500

    Add Notifications Helpers and Scope :posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-
+  #Mothods to use the variables of default edit view register
+  ####
   def resource_name
     :user
   end
@@ -15,6 +16,27 @@ module ApplicationHelper
   def resource_class
     devise_mapping.to
   end
+###
 
 
+  # mthods to Noticfications
+  ###
+  # Returns the new record created in notifications table
+  def new_notification(user, notice_id, notice_type)
+    notice = user.notifications.build(notice_id: notice_id, notice_type: notice_type)
+    user.notice_seen = false
+    user.save
+    notice
+  end
+  def notification_find(notice, type)
+    return User.find(notice.notice_id) if type == 'friendRequest'
+    return User.find(notice.notice_id) if type == 'like'
+    return Post.find(notice.notice_id) if type == 'post'
+    return Post.find(notice.notice_id) if type == 'comment'
+    return Post.find(notice.notice_id) if type == 'like-post'
+    return unless type == 'like-comment'
+    comment = Comment.find(notice.notice_id)
+    Post.find(comment.post_id)
+  end
+  ###
 end
