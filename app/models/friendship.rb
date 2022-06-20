8624092c76c79073b20dd0e75a30c88ecffc13f1
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 13:08:44 -0500

    Add relation of foreign_key to Friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :sent_to
-  belongs_to :sent_by
+  belongs_to :sent_to, class_name: 'User', foreign_key: 'sent_to_id'
+  belongs_to :sent_by, clas_name: 'User', foreign_key: 'sent_ny_id'
 end
