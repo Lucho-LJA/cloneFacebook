164ce604ca47fb021e73c5fae0ac6b529d955674
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 17:18:25 -0500

    UPdate ROutes to friendship

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: 'users/omniauth_callbacks'}
   
   resources :boards, only: %i[index show] do
-    resources :friendships, only: %i[create]
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
     resources :likes, only: %i[create]
   end
   resources :posts, only: %i[index new create show destroy] do
