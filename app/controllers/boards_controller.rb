3653bda54ed13cecd2f358a8f78bbd8ab784ed7c
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 16:14:23 -0500

    Add Boards controllers

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,9 @@
 class BoardsController < ApplicationController
   def index
+    @users = User.all
+    @friends = current_user.friends
+    @pending_requests = current_user.pending_requests
+    @friend_requests = current_user.received_requests
   end
 
   def show
