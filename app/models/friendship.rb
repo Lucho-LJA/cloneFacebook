9ada8e64adb19536274fd48be09c86bc30e8c720
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 13:11:16 -0500

    Adds scope to  Friendship and add :status column innto migration

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :sent_to, class_name: 'User', foreign_key: 'sent_to_id'
   belongs_to :sent_by, clas_name: 'User', foreign_key: 'sent_ny_id'
+  scope :friends, -> { where('status =?', true) }
+  scope :not_friends, -> { where('status =?', false) }
 end
