4138a322351c44f8541b313681269053eac63bad
Lucho-lja <ljallauca.a@gmail.com>
2022-06-20 20:51:54 -0500

    Add @notificatiolns

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,6 +4,7 @@ class BoardsController < ApplicationController
     @friends = current_user.friends
     @pending_requests = current_user.pending_requests
     @friend_requests = current_user.received_requests
+    @notifications = Notification.where(user_id:current_user.id)
   end
 
   def show
